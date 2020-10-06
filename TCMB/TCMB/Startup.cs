c6bd7d57a54074f7b1320f5d78bd9bf85994e215
Abdullah Kahriman <kahriman.abdullah@gmail.com>
2020-10-06 20:17:22 +0300

    startup configuration

--- TCMB/TCMB/Startup.cs
+++ TCMB/TCMB/Startup.cs
@@ -1,6 +1,7 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.SpaServices.AngularCli;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
@@ -50,6 +51,20 @@ namespace TCMB
 
             app.UseHttpsRedirection();
             app.UseMvc();
+
+            app.UseSpa(spa =>
+            {
+                //To learn more about options for serving an Angular SPA from ASP.NET Core,
+                //see https://go.microsoft.com/fwlink/?linkid=864501
+
+
+               spa.Options.SourcePath = "../../";
+
+                if (env.IsDevelopment())
+                {
+                    spa.UseAngularCliServer(npmScript: "start");
+                }
+            });
         }
     }
 }
